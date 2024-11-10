038c67b551bc9a85ba548cbb5b4ac5bae0633026
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-10 11:54:23 +0100

    Init

---
+++ src/main/java/ftn/siit/project/isspoject/IsspojectApplication.java
@@ -0,0 +1,13 @@
+package ftn.siit.project.isspoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsspojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsspojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftn/siit/project/isspoject/IsspojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.siit.project.isspoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsspojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
