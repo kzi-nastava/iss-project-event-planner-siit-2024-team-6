39f5ad8c3244c129a8267b9d422783c2a91827cf
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-18 17:38:16 +0100

    [ADD] Repository / Service style

--- src/main/java/ftn/siit/project/isspoject/repository/UserRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package ftn.siit.project.isspoject.repository;
 
-public interface UserRepository {
+import ftn.siit.project.isspoject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
---
+++ src/main/java/ftn/siit/project/isspoject/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package ftn.siit.project.isspoject.service;
+
+import ftn.siit.project.isspoject.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
