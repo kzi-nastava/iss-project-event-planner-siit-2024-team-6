4aa517b757e261591a08c68fbd62c90eb6385c1f
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-23 00:12:46 +0100

    [ADD] Organizer and person DTO's

---
+++ src/main/java/ftn/siit/project/isspoject/dto/OrganizerDTO.java
@@ -0,0 +1,8 @@
+package ftn.siit.project.isspoject.dto;
+
+import ftn.siit.project.isspoject.entity.Event;
+
+import java.util.List;
+
+public class OrganizerDTO extends UserDTO{
+}
---
+++ src/main/java/ftn/siit/project/isspoject/dto/ProviderDTO.java
@@ -0,0 +1,19 @@
+package ftn.siit.project.isspoject.dto;
+
+import ftn.siit.project.isspoject.entity.Offer;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ProviderDTO extends UserDTO{
+    private String companyEmail;
+    private String companyName;
+    private String companyAddress;
+    private String description;
+    private String[] companyPhotos;
+    private String openingTime;
+    private String closingTime;
+}
