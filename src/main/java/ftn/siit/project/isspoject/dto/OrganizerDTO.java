2dd77a7fa26e69bf6c422f9e57807d7f873b4291
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-23 14:57:32 +0100

    [EDIT] Added @Data annotation for OrganizerDTO

--- src/main/java/ftn/siit/project/isspoject/dto/OrganizerDTO.java
+++ src/main/java/ftn/siit/project/isspoject/dto/OrganizerDTO.java
@@ -1,8 +1,12 @@
 package ftn.siit.project.isspoject.dto;
 
 import ftn.siit.project.isspoject.entity.Event;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class OrganizerDTO extends UserDTO{
 }
