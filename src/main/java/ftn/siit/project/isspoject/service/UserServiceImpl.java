13a93ed3203d3247f521c536909f266bbd5df50f
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-23 14:32:03 +0100

    [EDIT] Implemented save method and added some minot changes

--- src/main/java/ftn/siit/project/isspoject/service/UserServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ftn.siit.project.isspoject.service;
 
 import ftn.siit.project.isspoject.dto.RegistrationRequestDTO;
+import ftn.siit.project.isspoject.entity.Provider;
 import ftn.siit.project.isspoject.entity.User;
 import ftn.siit.project.isspoject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,7 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> findAll() {
         User user1 = new User();
+        user1.setId(0);
         user1.setEmail("john.doe@gmail.com");
         user1.setPassword("123");
         user1.setPhotoUrl("https://example.com/photo.jpg");
@@ -53,7 +55,8 @@ public class UserServiceImpl implements UserService{
         user1.setAddress("221B Baker Street, London");
         user1.setPhoneNumber("+44 20 7946 0958");
 
-        User user2 = new User();
+        User user2 = new Provider();
+        user2.setId(1);
         user2.setEmail("jane.smith@example.com");
         user2.setPassword("456");
         user2.setPhotoUrl("https://example.com/jane.jpg");
@@ -63,7 +66,7 @@ public class UserServiceImpl implements UserService{
         user2.setLastname("Smith");
         user2.setAddress("742 Evergreen Terrace, Springfield");
         user2.setPhoneNumber("+1 555-123-4567");
-
+        ((Provider) user2).setCompanyName("company name");
         List<User> users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
@@ -83,11 +86,11 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User findById(Integer id) {
-        return null;
+        return findAll().get(id);
     }
 
     @Override
     public void save(User user) {
-
+        System.out.println(user);
     }
 }
