e5065941df1e57f4accad977529ca8a41db06ba9
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-24 15:10:55 +0100

    [ADD] EventDTO

--- src/main/java/ftn/siit/project/isspoject/dto/EventDTO.java
+++ src/main/java/ftn/siit/project/isspoject/dto/EventDTO.java
@@ -1,15 +1,12 @@
-package ftn.siit.project.isspoject.entity;
+package ftn.siit.project.isspoject.dto;
 
-import jakarta.persistence.*;
+import ftn.siit.project.isspoject.entity.EventType;
 import lombok.Data;
+
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
-//@Entity
-public class Event {
-//    @Id
-    private Integer id;
+public class EventDTO {
     private String name;
     private String description;
     private Integer maxParticipants;
@@ -18,6 +15,4 @@ public class Event {
     private String place;
     private LocalDateTime date;
     private EventType eventType;
-    private Organizer organizer;
-    private List<Activity> activities;
 }
