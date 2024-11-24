94f35ddf2f8468b6991882c55b9e642191aa7b1e
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-24 15:20:41 +0100

    [ADD] methods overrided

--- src/main/java/ftn/siit/project/isspoject/service/EventService.java
+++ src/main/java/ftn/siit/project/isspoject/service/EventService.java
@@ -1,4 +1,16 @@
 package ftn.siit.project.isspoject.service;
 
+import ftn.siit.project.isspoject.entity.Event;
+import ftn.siit.project.isspoject.entity.Organizer;
+
+import java.util.List;
+
 public interface EventService {
+    void save(Event event);
+
+    List<Event> findByOrganizer(Organizer organizer);
+
+    Event findById(Integer eventId);
+
+    void delete(Event event);
 }
--- src/main/java/ftn/siit/project/isspoject/service/EventServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/EventServiceImpl.java
@@ -1,4 +1,30 @@
 package ftn.siit.project.isspoject.service;
 
+import ftn.siit.project.isspoject.entity.Event;
+import ftn.siit.project.isspoject.entity.Organizer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EventServiceImpl implements EventService{
+    @Override
+    public void save(Event event) {
+
+    }
+
+    @Override
+    public List<Event> findByOrganizer(Organizer organizer) {
+        return null;
+    }
+
+    @Override
+    public Event findById(Integer eventId) {
+        return null;
+    }
+
+    @Override
+    public void delete(Event event) {
+
+    }
 }
