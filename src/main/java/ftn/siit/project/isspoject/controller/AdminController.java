c638d312d82e3f99e5cc1fba0ba802c67cc5098b
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-24 13:47:41 +0100

    [ADD] AdminController class

---
+++ src/main/java/ftn/siit/project/isspoject/controller/AdminController.java
@@ -0,0 +1,9 @@
+package ftn.siit.project.isspoject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/users")
+public class AdminController {
+}
