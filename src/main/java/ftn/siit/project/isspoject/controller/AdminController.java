5bb713cd292ca3761b435760810695009b8e7206
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-24 14:08:08 +0100

    [ADD] events/add method

--- src/main/java/ftn/siit/project/isspoject/controller/AdminController.java
+++ src/main/java/ftn/siit/project/isspoject/controller/AdminController.java
@@ -1,9 +1,37 @@
 package ftn.siit.project.isspoject.controller;
 
+import ftn.siit.project.isspoject.dto.EventTypeDTO;
+import ftn.siit.project.isspoject.entity.EventType;
+import ftn.siit.project.isspoject.service.EventTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/users")
+@RequestMapping(value = "api/admin")
 public class AdminController {
+    @Autowired
+    private EventTypeService eventTypeService;
+
+    @PostMapping("events/add")
+    public ResponseEntity<String> addEventType(@RequestBody EventTypeDTO eventTypeDTO) {
+        if (eventTypeDTO == null || eventTypeDTO.getName() == null || eventTypeDTO.getDescription() == null) {
+            return new ResponseEntity<>("Invalid event type data", HttpStatus.BAD_REQUEST);
+        }
+
+        EventType eventType = new EventType();
+        eventType.setName(eventTypeDTO.getName());
+        eventType.setDescription(eventTypeDTO.getDescription());
+        eventType.setCategories(eventTypeDTO.getCategories());
+        eventType.setDeleted(false);
+
+        eventTypeService.save(eventType);
+        return new ResponseEntity<>("Event type added successfully", HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/ftn/siit/project/isspoject/service/EventTypeService.java
+++ src/main/java/ftn/siit/project/isspoject/service/EventTypeService.java
@@ -1,4 +1,7 @@
 package ftn.siit.project.isspoject.service;
 
+import ftn.siit.project.isspoject.entity.EventType;
+
 public interface EventTypeService {
+    void save(EventType eventType);
 }
