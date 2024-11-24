7865a9f192151f8015b7d19a373bc5b9401ec38c
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-24 14:00:55 +0100

    [ADD] EventTypeDTO

---
+++ src/main/java/ftn/siit/project/isspoject/dto/EventTypeDTO.java
@@ -0,0 +1,15 @@
+package ftn.siit.project.isspoject.dto;
+
+import ftn.siit.project.isspoject.entity.Event;
+import ftn.siit.project.isspoject.entity.Offer;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class EventTypeDTO {
+    private String name;
+    private String description;
+    private boolean isDeleted;
+    private List<Offer> offers;
+}
