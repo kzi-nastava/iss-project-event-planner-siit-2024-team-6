f67e50d2aa9be636be2be81f5444a9ad3b03295f
DusicaPesic <dusicapesich@gmail.com>
2024-11-24 22:42:44 +0100

    [ADD] dto, repository, service and controller for Notification class

---
+++ src/main/java/ftn/siit/project/isspoject/service/NotificationServiceImpl.java
@@ -0,0 +1,43 @@
+package ftn.siit.project.isspoject.service;
+
+import ftn.siit.project.isspoject.entity.Notification;
+import ftn.siit.project.isspoject.entity.User;
+import ftn.siit.project.isspoject.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+
+    @Override
+    public Notification findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Notification> findByRecipient(User recipient) {
+        return List.of();
+    }
+
+    @Override
+    public void save(Notification notification) {
+
+    }
+
+    @Override
+    public void notifyUsers(List<User> users, String message) {
+        for (User user : users) {
+            Notification notification = new Notification();
+            notification.setReceiver(user);
+            notification.setText(message);
+            notification.setTimestamp(LocalDateTime.now());
+            this.save(notification);
+        }
+    }
+}
