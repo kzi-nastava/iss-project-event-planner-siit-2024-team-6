013803cdb366b9648a032e10423e6f115b362267
DusicaPesic <dusicapesich@gmail.com>
2024-11-25 15:33:52 +0100

    [ADD] dto, repository, service and controller for Reservation class

---
+++ src/main/java/ftn/siit/project/isspoject/controller/ReservationController.java
@@ -0,0 +1,29 @@
+package ftn.siit.project.isspoject.controller;
+
+import ftn.siit.project.isspoject.dto.ReservationDTO;
+import ftn.siit.project.isspoject.entity.Reservation;
+import ftn.siit.project.isspoject.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/reservations")
+public class ReservationController {
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> createReservation(@RequestBody ReservationDTO dto) {
+        Reservation reservation = reservationService.save(dto);
+        sendConfirmations(reservation);
+        return ResponseEntity.ok("Service with id " + reservation.getServiceId() + " is reserved for event "+ reservation.getEventId());
+    }
+
+    private void sendConfirmations(Reservation reservation) {
+
+    }
+}
---
+++ src/main/java/ftn/siit/project/isspoject/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package ftn.siit.project.isspoject.repository;
+
+import ftn.siit.project.isspoject.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+}
