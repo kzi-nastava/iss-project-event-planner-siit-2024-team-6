7486a60177fb02263c46a6f1bc896e4369815945
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-11-26 00:43:11 +0100

    [ADD] backend base for history of offers
    Implementation of model and service classes for offer history.

---
+++ src/main/java/ftn/siit/project/isspoject/entity/OfferHistory.java
@@ -0,0 +1,10 @@
+package ftn.siit.project.isspoject.entity;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OfferHistory {
+    Integer id;
+    List<Offer> offers;
+    List<ZonedDateTime> timestamps;
+}
