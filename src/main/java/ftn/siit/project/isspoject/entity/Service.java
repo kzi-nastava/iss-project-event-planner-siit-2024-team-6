430f1b54151301079b06adfd4fa31492ba797c06
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-26 11:46:31 +0100

    [ADD] Service.java

---
+++ src/main/java/ftn/siit/project/isspoject/entity/Service.java
@@ -0,0 +1,17 @@
+package ftn.siit.project.isspoject.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+//@Entity
+public class Service extends Offer{
+
+    private String specifics;
+    private int minDuration;
+    private int maxDuration;
+    private int preciseDuration;
+    private int latestReservation;
+    private int latestCancelation;
+}
