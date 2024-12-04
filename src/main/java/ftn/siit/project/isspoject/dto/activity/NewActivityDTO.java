2b233526457a429cb059563ed7f86322ab9fa1a8
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-04 18:01:13 +0100

    [ADD] Refactored NewActivityDTO

---
+++ src/main/java/ftn/siit/project/isspoject/dto/activity/NewActivityDTO.java
@@ -0,0 +1,15 @@
+package ftn.siit.project.isspoject.dto.activity;
+
+import ftn.siit.project.isspoject.entity.Event;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class NewActivityDTO {
+    private String name;
+    private String description;
+    private String location;
+    private LocalDateTime start;
+    private LocalDateTime end;
+}
