fe6863c34bc91c94000bc7563c83e9aabf2dc9db
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-04 18:09:53 +0100

    [ADD] NewEventDTO

---
+++ src/main/java/ftn/siit/project/isspoject/dto/event/NewEventDTO.java
@@ -0,0 +1,34 @@
+package ftn.siit.project.isspoject.dto.event;
+
+import ftn.siit.project.isspoject.entity.Event;
+import ftn.siit.project.isspoject.entity.EventType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class NewEventDTO {
+    private String name;
+    private String description;
+    private Integer maxParticipants;
+    private Integer participants;
+    private Boolean isPublic;
+    private String place;
+    private LocalDateTime date;
+    private EventType eventType;
+
+    public NewEventDTO() {}
+    public NewEventDTO(Event event) {
+        if (event == null) {
+            throw new IllegalArgumentException("Event cannot be null");
+        }
+        this.name = event.getName();
+        this.description = event.getDescription();
+        this.maxParticipants = event.getMaxParticipants();
+        this.participants = event.getParticipants();
+        this.isPublic = event.getIsPublic();
+        this.place = event.getPlace();
+        this.date = event.getDate();
+        this.eventType = event.getEventType();
+    }
+}
