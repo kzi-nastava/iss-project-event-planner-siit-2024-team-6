6e22da368993d53472dee207a133899aa5fe916e
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-04 17:43:07 +0100

    [ADD] Annotations for Block

--- src/main/java/ftn/siit/project/isspoject/entity/Block.java
+++ src/main/java/ftn/siit/project/isspoject/entity/Block.java
@@ -2,9 +2,21 @@ package ftn.siit.project.isspoject.entity;
 
 import lombok.Data;
 
+import jakarta.persistence.*;
 @Data
+@Entity
+@Table(name = "block")
 public class Block {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "blocker_id", nullable = false)
     private User blockerId;
+
+    @ManyToOne
+    @JoinColumn(name = "blocked_id", nullable = false)
     private User blockedId;
 }
