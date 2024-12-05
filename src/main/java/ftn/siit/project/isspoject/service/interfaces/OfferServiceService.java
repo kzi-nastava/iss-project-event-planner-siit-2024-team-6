f53c1158fafa99dc245ac86880772aa6cf786c5b
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-05 09:52:19 +0100

    [ADD] service for OfferService

---
+++ src/main/java/ftn/siit/project/isspoject/service/interfaces/OfferServiceService.java
@@ -0,0 +1,6 @@
+package ftn.siit.project.isspoject.service.interfaces;
+import ftn.siit.project.isspoject.entity.OfferService;
+
+public interface OfferServiceService {
+    OfferService findById(int id);
+}
