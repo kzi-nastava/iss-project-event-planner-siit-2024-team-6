fca87ba19e3b36e32193940cdc3c31a0a706ccc7
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-05 09:52:01 +0100

    [UPDATE] services with attributes of respectable repositories

--- src/main/java/ftn/siit/project/isspoject/service/implementations/OrganizerServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/OrganizerServiceImpl.java
@@ -1,7 +1,9 @@
 package ftn.siit.project.isspoject.service.implementations;
 
 import ftn.siit.project.isspoject.entity.Organizer;
+import ftn.siit.project.isspoject.repository.OfferServiceRepository;
 import ftn.siit.project.isspoject.service.interfaces.OrganizerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
