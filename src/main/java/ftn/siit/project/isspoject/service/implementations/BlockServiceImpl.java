26c2c5ac878c7d1d14602ad23885bb3d5e55e943
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-05 12:17:58 +0100

    [ADD] BlockService

---
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/BlockServiceImpl.java
@@ -0,0 +1,7 @@
+package ftn.siit.project.isspoject.service.implementations;
+
+import ftn.siit.project.isspoject.service.interfaces.BlockService;
+
+public class BlockServiceImpl implements BlockService {
+
+}
