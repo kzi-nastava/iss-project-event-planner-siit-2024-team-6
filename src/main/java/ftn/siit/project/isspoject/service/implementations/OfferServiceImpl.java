676cc05ff310bfbe2e70418e078ea3cfc63d4b0f
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-05 13:06:27 +0100

    [UPDATE] services to call repositories

--- src/main/java/ftn/siit/project/isspoject/service/implementations/OfferServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/OfferServiceImpl.java
@@ -50,29 +50,7 @@ public class OfferServiceImpl implements OfferService {
     @Override
     public List<Offer> findAll() {
 
-        return List.of(
-                new Offer(1, Status.ACCEPTED, "Offer 1", "Description 1", 100.0, 10.0,
-                        List.of("photo1.jpg", "photo2.jpg"), true, true, false,
-                        LocalDateTime.now(), new Category(), List.of(new EventType())),
-                new Offer(2, Status.ACCEPTED, "Offer 2", "Description 2", 200.0, 20.0,
-                        List.of("photo3.jpg", "photo4.jpg"), true, false, false,
-                        LocalDateTime.now().plusDays(1), new Category(), List.of(new EventType())),
-                new Offer(3, Status.ACCEPTED, "Offer 3", "Description 3", 300.0, 30.0,
-                        List.of("photo5.jpg", "photo6.jpg"), true, true, false,
-                        LocalDateTime.now().plusDays(2), new Category(), List.of(new EventType())),
-                new Offer(4, Status.ACCEPTED, "Offer 4", "Description 4", 400.0, 40.0,
-                        List.of("photo7.jpg", "photo8.jpg"), false, false, false,
-                        LocalDateTime.now().plusDays(3), new Category(), List.of(new EventType())),
-                new Offer(5, Status.ACCEPTED, "Offer 5", "Description 5", 500.0, 50.0,
-                        List.of("photo9.jpg", "photo10.jpg"), true, true, false,
-                        LocalDateTime.now().plusDays(4), new Category(), List.of(new EventType())),
-                new Offer(6, Status.PENDING, "Offer 6", "Description 6", 600.0, 60.0,
-                        List.of("photo11.jpg", "photo12.jpg"), true, false, false,
-                        LocalDateTime.now().plusDays(5), new Category(), List.of(new EventType())),
-                new Offer(7, Status.ACCEPTED, "Offer 7", "Description 7", 700.0, 70.0,
-                        List.of("photo13.jpg", "photo14.jpg"), true, true, false,
-                        LocalDateTime.now().plusDays(6), new Category(), List.of(new EventType()))
-        );
+        return offerRepository.findAll();
     }
 
     @Override
