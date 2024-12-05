afecd471579f19516b1d106bb74e61388af14333
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-05 10:59:18 +0100

    [ADD] BasicRepositories

--- src/main/java/ftn/siit/project/isspoject/repository/ReportRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/ReportRepository.java
@@ -1,11 +1,9 @@
 package ftn.siit.project.isspoject.repository;
 
-import ftn.siit.project.isspoject.entity.User;
+import ftn.siit.project.isspoject.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
+public interface ReportRepository extends JpaRepository<Report, Integer> {
 }
-
