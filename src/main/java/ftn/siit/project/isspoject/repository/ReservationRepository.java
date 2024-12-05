45a3aaacc84bd9b5af9539f927baf38c8f233903
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-05 02:52:50 +0100

    [UPDATE] repository and service for Reservation class to work with database

--- src/main/java/ftn/siit/project/isspoject/repository/ReservationRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/ReservationRepository.java
@@ -4,7 +4,6 @@ import ftn.siit.project.isspoject.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-// extends JpaRepository<Reservation, Integer>
-public interface ReservationRepository{
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
 }
