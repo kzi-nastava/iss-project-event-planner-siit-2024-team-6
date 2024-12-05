6dfeaee6bb1fa2c2ec3cf0592bbfbf0d17d363bd
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-05 02:55:19 +0100

    [UPDATE] repository and service for Message class to work with database

--- src/main/java/ftn/siit/project/isspoject/service/implementations/MessageServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/MessageServiceImpl.java
@@ -2,9 +2,13 @@ package ftn.siit.project.isspoject.service.implementations;
 
 import ftn.siit.project.isspoject.dto.message.NewMessageDTO;
 import ftn.siit.project.isspoject.entity.Message;
+import ftn.siit.project.isspoject.entity.User;
+import ftn.siit.project.isspoject.repository.MessageRepositoy;
 import ftn.siit.project.isspoject.service.interfaces.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,27 +17,35 @@ import java.util.stream.Collectors;
 @Service
 public class MessageServiceImpl implements MessageService {
 
+
+    @Autowired
+    private MessageRepositoy messageRepositoy;
     //public Page<Message> findAll(Pageable page) {
 //        return messageRepository.findAll(page);
 //    }
 
     @Override
     public Message save(Message message) {
-        return null;
+        return messageRepositoy.save(message);
     }
 
     @Override
     public void delete(Message message) {
-       return;
+        messageRepositoy.delete(message);
     }
 
     @Override
-    public Message save(NewMessageDTO newMessageDTO, int senderId, int recieverId) {
-        return null;
+    public Message save(NewMessageDTO newMessageDTO, User sender, User receiver) {
+        Message message = new Message();
+        message.setSender(sender);
+        message.setReceiver(receiver);
+        message.setText(newMessageDTO.getText());
+        message.setTime(LocalDateTime.now());
+        return save(message);
     }
 
     @Override
     public List<Message> findMessagessBetween(Integer user1, Integer user2) {
-        return null;
+        return messageRepositoy.findMessagesBetweenUsers(user1, user2);
     }
 }
--- src/main/java/ftn/siit/project/isspoject/service/interfaces/MessageService.java
+++ src/main/java/ftn/siit/project/isspoject/service/interfaces/MessageService.java
@@ -2,12 +2,13 @@ package ftn.siit.project.isspoject.service.interfaces;
 
 import ftn.siit.project.isspoject.dto.message.NewMessageDTO;
 import ftn.siit.project.isspoject.entity.Message;
+import ftn.siit.project.isspoject.entity.User;
 
 import java.util.List;
 
 public interface MessageService {
     Message save(Message message);
     void delete(Message message);
-    Message save(NewMessageDTO newMessageDTO, int senderId, int recieverId);
+    Message save(NewMessageDTO newMessageDTO, User sender, User receiver);
     List<Message> findMessagessBetween(Integer user1, Integer user2);
 }
