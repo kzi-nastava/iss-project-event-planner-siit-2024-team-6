97be8fd5207a5390ce7237594d7d3733eea74c0a
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-09 16:56:42 +0100

    [ADD] CRUD methods to ActivityService

--- src/main/java/ftn/siit/project/isspoject/service/implementations/ActivityServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/ActivityServiceImpl.java
@@ -1,13 +1,37 @@
 package ftn.siit.project.isspoject.service.implementations;
 
+import ftn.siit.project.isspoject.entity.Activity;
+import ftn.siit.project.isspoject.exceptions.NotFoundException;
 import ftn.siit.project.isspoject.repository.ActivityRepository;
 import ftn.siit.project.isspoject.service.interfaces.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActivityServiceImpl implements ActivityService {
 
     @Autowired
     private ActivityRepository activityRepository;
+
+    @Override
+    public List<Activity> findAll() {
+        return activityRepository.findAll();
+    }
+
+    @Override
+    public Activity findById(Integer id) {
+        return activityRepository.findById(id).orElseThrow(() -> new NotFoundException("Activity not found with ID: " + id));
+    }
+
+    @Override
+    public Activity save(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Override
+    public void delete(Activity activity) {
+
+    }
 }
--- src/main/java/ftn/siit/project/isspoject/service/interfaces/ActivityService.java
+++ src/main/java/ftn/siit/project/isspoject/service/interfaces/ActivityService.java
@@ -1,4 +1,17 @@
 package ftn.siit.project.isspoject.service.interfaces;
 
+import ftn.siit.project.isspoject.dto.event.NewClosedEventDTO;
+import ftn.siit.project.isspoject.entity.Activity;
+import ftn.siit.project.isspoject.entity.Event;
+import ftn.siit.project.isspoject.entity.Organizer;
+
+import java.util.List;
+
 public interface ActivityService {
+
+    List<Activity> findAll();
+    //Page<Activity> findAll(Pageable page);
+    Activity findById(Integer id);
+    Activity save(Activity activity);
+    void delete(Activity activity);
 }
