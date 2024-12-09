2b44964312deaf15e605571cafe2091474437db5
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-09 23:45:47 +0100

    [FIX] imports

--- src/main/java/ftn/siit/project/isspoject/repository/ReservationRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/ReservationRepository.java
@@ -1,7 +1,6 @@
 package ftn.siit.project.isspoject.repository;
 
 import ftn.siit.project.isspoject.entity.Event;
-import ftn.siit.project.isspoject.entity.OfferService;
 import ftn.siit.project.isspoject.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
