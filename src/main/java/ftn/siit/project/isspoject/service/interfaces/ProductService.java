bede0a8cafa9081e76b516eeaff254f90f5cf41f
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-09 18:13:44 +0100

    [UPDATE] interfaces of service classes
    Added method's signatures

--- src/main/java/ftn/siit/project/isspoject/service/interfaces/ProductService.java
+++ src/main/java/ftn/siit/project/isspoject/service/interfaces/ProductService.java
@@ -6,12 +6,11 @@ import java.util.List;
 
 public interface ProductService {
     Product save(Product product);
-
-    List<Product> findByProvider(Integer providerId);
-
+    Product update(Product product);
+    void delete(Product product);
     List<Product> searchByName(String name);
     List<Product> findAll();
     List<Product> filterProducts(String category, String eventType, Double minPrice, Double maxPrice, Boolean available, Boolean visible);
-
     Product findById(Integer productId);
+    List<Product> findByProvider(int providerId);
 }
