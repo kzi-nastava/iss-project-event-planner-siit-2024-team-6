daac14474de7d019aa6bec738b9feb957220e137
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-12 23:07:20 +0100

    [ADD] GetRole method in repository (for security and jwt)

--- src/main/java/ftn/siit/project/isspoject/repository/UserRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/UserRepository.java
@@ -2,6 +2,8 @@ package ftn.siit.project.isspoject.repository;
 
 import ftn.siit.project.isspoject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.userType FROM User u WHERE u.id = :userId")
+    String findUserTypeById(@Param("userId") Integer userId);
 }
 
