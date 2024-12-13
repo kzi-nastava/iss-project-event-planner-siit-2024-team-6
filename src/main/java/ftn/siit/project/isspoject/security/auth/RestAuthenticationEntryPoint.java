bf4367bec1ac72b992d6a7b1bdacbdb18dc8f403
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-13 12:37:15 +0100

    [ADD] implemented RestAuthenticationEntryPoint

--- src/main/java/ftn/siit/project/isspoject/security/auth/RestAuthenticationEntryPoint.java
+++ src/main/java/ftn/siit/project/isspoject/security/auth/RestAuthenticationEntryPoint.java
@@ -1,4 +1,19 @@
 package ftn.siit.project.isspoject.security.auth;
 
-public class RestAuthenticationEntryPoint {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    //Metoda koja se izvrsava ukoliko za prosledjene kredencijale korisnik pokusa da pristupi zasticenom REST servisu
+    //Metoda vraca 401 Unauthorized response, ukoliko postoji Login Page u aplikaciji, pozeljno je da se korisnik redirektuje na tu stranicu !!!
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
 }
