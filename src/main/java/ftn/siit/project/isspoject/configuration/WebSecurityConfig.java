8d254e960cf2c80bff51afa7967bbfe516a9cd56
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-14 16:04:43 +0100

    [ADD] permissions

--- src/main/java/ftn/siit/project/isspoject/configuration/WebSecurityConfig.java
+++ src/main/java/ftn/siit/project/isspoject/configuration/WebSecurityConfig.java
@@ -98,7 +98,9 @@ public class WebSecurityConfig{
         http.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(restAuthenticationEntryPoint));
         http.authorizeHttpRequests(request -> {
             request.requestMatchers(new AntPathRequestMatcher("/api/users/login")).permitAll()
-                    .requestMatchers(new AntPathRequestMatcher("/api/users")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/api/users/**")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/api/events/**")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/api/offers/**")).permitAll()
                     // Разрешаем доступ к маршруту /error для более удобных сообщений об ошибках.
                     .requestMatchers(new AntPathRequestMatcher("/error")).permitAll()
                     //.requestMatchers(new AntPathRequestMatcher("/api/whoami")).hasRole("USER")
