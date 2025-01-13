31be2da75fcd63386c4c4863094fd185ef514626
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-01-13 13:40:40 +0100

    [UPDATE] attributes in CategorySuggestion classes

--- src/main/java/ftn/siit/project/isspoject/dto/category/CategorySuggestionDTO.java
+++ src/main/java/ftn/siit/project/isspoject/dto/category/CategorySuggestionDTO.java
@@ -3,17 +3,23 @@ package ftn.siit.project.isspoject.dto.category;
 import ftn.siit.project.isspoject.entity.CategorySuggestion;
 import ftn.siit.project.isspoject.entity.Report;
 import ftn.siit.project.isspoject.entity.Status;
+import lombok.Data;
 
+@Data
 public class CategorySuggestionDTO {
     Integer id;
-    String suggestion;
-    String status;
+    String name;
+    String description;
+    String offerName;
+    String offerDescription;
 
     public CategorySuggestionDTO() {}
 
     public CategorySuggestionDTO(CategorySuggestion cs) {
         this.id = cs.getId();
-        this.suggestion = cs.getSuggestion();
-        this.status = cs.getStatus().toString();
+        this.name = cs.getName();
+        this.description = cs.getDescription();
+        this.offerName = cs.getOffer().getName();
+        this.offerDescription = cs.getOffer().getDescription();
     }
 }
--- src/main/java/ftn/siit/project/isspoject/entity/CategorySuggestion.java
+++ src/main/java/ftn/siit/project/isspoject/entity/CategorySuggestion.java
@@ -1,5 +1,6 @@
 package ftn.siit.project.isspoject.entity;
 
+import ftn.siit.project.isspoject.dto.category.NewCategoryDTO;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -10,15 +11,17 @@ public class CategorySuggestion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
-    String suggestion;
+    String name;
+    String description;
     Status status;
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "offer_id", referencedColumnName = "id")
     Offer offer;
 
     public CategorySuggestion(){}
-    public CategorySuggestion(String suggestion, Status status, Offer offer) {
-        this.suggestion = suggestion;
+    public CategorySuggestion(NewCategoryDTO suggestion, Status status, Offer offer) {
+        this.name = suggestion.getName();
+        this.description = suggestion.getDescription();
         this.status = status;
         this.offer = offer;
     }
