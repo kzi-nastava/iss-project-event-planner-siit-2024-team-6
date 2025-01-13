6164a23c1a2977a53a10b627bf28b17f4bad5c5c
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-01-13 13:37:34 +0100

    [UPDATE] methods
    New methods for approving, rejecting and updating a suggestion have been added, while unnecessary methods have been removed.

--- src/main/java/ftn/siit/project/isspoject/service/interfaces/CategorySuggestionService.java
+++ src/main/java/ftn/siit/project/isspoject/service/interfaces/CategorySuggestionService.java
@@ -1,14 +1,19 @@
 package ftn.siit.project.isspoject.service.interfaces;
 
-import ftn.siit.project.isspoject.dto.category.NewCategorySuggestionDTO;
+import ftn.siit.project.isspoject.dto.category.NewCategoryDTO;
 import ftn.siit.project.isspoject.entity.CategorySuggestion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CategorySuggestionService {
     List<CategorySuggestion> getPending();
+    Page<CategorySuggestion> getPending(Pageable pageable);
     CategorySuggestion findById(int id);
     CategorySuggestion save(CategorySuggestion categorySuggestion);
     CategorySuggestion update(CategorySuggestion categorySuggestion);
-    void delete(CategorySuggestion categorySuggestion);
+    CategorySuggestion update(int id, NewCategoryDTO dto);
+    CategorySuggestion approve(int id);
+    CategorySuggestion reject(int id);
 }
