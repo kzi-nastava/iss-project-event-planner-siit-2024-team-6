ca44c0ff3ed8e026768a92d667016cff9f35dc2b
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-09 21:03:23 +0200

    [UPDATE] query to order by last updated

--- src/main/java/ftn/siit/project/isspoject/repository/ChatRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/ChatRepository.java
@@ -8,16 +8,24 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
     @Query("""
-    SELECT c FROM Chat c 
-    WHERE (c.participant1.id = :userId OR c.participant2.id = :userId) 
-      AND c.isBlocked = false
-    """)
+            SELECT c FROM Chat c 
+            WHERE (c.participant1.id = :userId OR c.participant2.id = :userId) 
+              AND c.isBlocked = false
+            ORDER BY c.lastUpdated DESC
+            """)
     List<Chat> findAllWithParticipant(@Param("userId") int userId);
+
+
+    @Query("SELECT c FROM Chat c WHERE " +
+            "(c.participant1.id = :userId1 AND c.participant2.id = :userId2) OR " +
+            "(c.participant1.id = :userId2 AND c.participant2.id = :userId1)")
+    Optional<Chat> findChatByParticipants(@Param("userId1") int userId1, @Param("userId2") int userId2);
 
 
 }
