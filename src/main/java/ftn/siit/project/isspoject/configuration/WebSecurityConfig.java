f6f49151567a41120dc0c9489e7f4d2bfc8138ba
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-09 23:16:15 +0200

    [UPDATE] security configuration

--- src/main/java/ftn/siit/project/isspoject/configuration/WebSecurityConfig.java
+++ src/main/java/ftn/siit/project/isspoject/configuration/WebSecurityConfig.java
@@ -135,6 +135,7 @@ public class WebSecurityConfig{
                     .requestMatchers(new AntPathRequestMatcher("/api/providers/product")).hasAnyRole("ADMIN","PROVIDER")
                     .requestMatchers(new AntPathRequestMatcher("/api/messages**")).authenticated()
                     .requestMatchers(new AntPathRequestMatcher("/api/providers/my-products")).hasRole("PROVIDER")
+                    .requestMatchers(new AntPathRequestMatcher("/socket/**")).permitAll()
 //                    .requestMatchers(new AntPathRequestMatcher("/api/users")).permitAll()
 //                    .requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll() my-products unPagedFavorites
 //            event-types-by-category-name
@@ -167,6 +168,7 @@ public class WebSecurityConfig{
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200" , "http://192.168.1.3:8000"));
         configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE", "PATCH")); // или просто "*"
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
