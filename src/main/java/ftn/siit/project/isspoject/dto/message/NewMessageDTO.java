054008216294d383468529585ab441687cf3b657
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-09 19:10:46 +0200

    [DELETE] time field

--- src/main/java/ftn/siit/project/isspoject/dto/message/NewMessageDTO.java
+++ src/main/java/ftn/siit/project/isspoject/dto/message/NewMessageDTO.java
@@ -8,11 +8,9 @@ import java.time.LocalDateTime;
 @Data
 public class NewMessageDTO {
     private String text;
-    private LocalDateTime time;
 
     NewMessageDTO(){}
     NewMessageDTO(Message message) {
         this.text = message.getText();
-        this.time = LocalDateTime.now();
     }
 }
