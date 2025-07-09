dcd5c1a7d13849a02721cba66a613dcd2749f49b
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-07-09 15:56:04 +0200

    [ADD] method to repository

--- src/main/java/ftn/siit/project/isspoject/repository/BudgetRepository.java
+++ src/main/java/ftn/siit/project/isspoject/repository/BudgetRepository.java
@@ -2,8 +2,14 @@ package ftn.siit.project.isspoject.repository;
 
 import ftn.siit.project.isspoject.entity.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Integer>{
+    @Query("SELECT b FROM Budget b LEFT JOIN FETCH b.budgetItems WHERE b.id = :id")
+    Optional<Budget> findByIdWithItems(@Param("id") Integer id);
 }
