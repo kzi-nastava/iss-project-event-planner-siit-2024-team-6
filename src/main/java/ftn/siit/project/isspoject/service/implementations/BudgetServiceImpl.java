76e32fbfc4361f234341aff967862e016720d75b
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-07-09 17:03:06 +0200

    [ADD] error for going over budget

--- src/main/java/ftn/siit/project/isspoject/service/implementations/BudgetServiceImpl.java
+++ src/main/java/ftn/siit/project/isspoject/service/implementations/BudgetServiceImpl.java
@@ -132,6 +132,9 @@ public class BudgetServiceImpl implements BudgetService {
         Budget b = findById(budgetId);
         for(BudgetItem bi : b.getBudgetItems()) {
             if(bi.getCategory().equals(category)){
+                if (bi.getCurrPrice() + price > bi.getMaxPrice()) {
+                    throw new IllegalArgumentException("You do not have enough allocated funds to make this purchase.");
+                }
                 bi.setCurrPrice(bi.getCurrPrice() + price);
                 return update(b);
             }
